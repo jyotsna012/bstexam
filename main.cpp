63f5a958b84a943ee187b8a469c166bbe2068dcf
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-06-03 10:12:39 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "bst.h"
+
+int main()
+{
+    bst object;
+    object.build(); 	//builds a BST
+    object.display();	//displays a BST
+
+    /*  PLACE YOUR FUNCTION CALL HERE */
+
+
+
+    object.display();	//displays again after!
+   
+    return 0;
+}
