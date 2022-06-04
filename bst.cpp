c147142e7f8464000c21c03077f94a71467b7dd6
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-06-03 18:31:33 -0700

    Update bst.cpp

--- bst.cpp
+++ bst.cpp
@@ -65,3 +65,45 @@ void bst::display() {
   display_tree(root, 0);
   cout << "--------------------------------" << endl;
 }
+
+void bst::findPreSuc(node* root, node*& pre, node*& suc, int key)
+{
+    // Base case
+    if (root == NULL)  return ;
+ 
+    // If key is present at root
+    if (root->key == key)
+    {
+        // the maximum value in left subtree is predecessor
+        if (root->left != NULL)
+        {
+            Node* tmp = root->left;
+            while (tmp->right)
+                tmp = tmp->right;
+            pre = tmp ;
+        }
+ 
+        // the minimum value in right subtree is successor
+        if (root->right != NULL)
+        {
+            Node* tmp = root->right ;
+            while (tmp->left)
+                tmp = tmp->left ;
+            suc = tmp ;
+        }
+        return ;
+    }
+ 
+    // If key is smaller than root's key, go to left subtree
+    if (root->key > key)
+    {
+        suc = root ;
+        findPreSuc(root->left, pre, suc, key) ;
+    }
+    else // go to right subtree
+    {
+        pre = root ;
+        findPreSuc(root->right, pre, suc, key) ;
+    }
+}
+ 
