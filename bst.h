734897621db4eebaca6acc4b1f9f591cff68f5e7
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-06-07 06:29:51 -0700

    Update bst.h

--- bst.h
+++ bst.h
@@ -25,7 +25,7 @@ class bst
 	void add_new(node* &root, node* current, node* parent, int number);
 	void findMinAdd(node* root);
 	void removeSmallest(node* root);
-	void deleteTree(node* node);
+	void deleteTree(node* root);
 
 /* ************** PLACE YOUR PROTOTYPE HERE ***************** */
  
