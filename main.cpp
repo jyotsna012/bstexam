90582ecb4b6d029a15e93de0e833e4cda34dbb0b
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-06-07 06:15:31 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
         
     node* root = object.getRoot();
     int val = root -> data;
-    object.removeAll(root, val);
+    object.removeAll(root, 5000);
     //object.add_new(root, root, root, 15);
     //object.inOrderTraversal(root);
     //bool result = object.search(root, 12);
