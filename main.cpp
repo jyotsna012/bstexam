831ea05515ff7940bae5fbc7617c6d4f17e728fe
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-06-07 10:26:16 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@ int main()
     object.display();	//displays a BST
         
     node* root = object.getRoot();
-    object.deleteTree(root, root);
+    root = root -> right;
+    object.allBut(root);
+    //object.deleteTree(root, root);
     //object.add_new(root, root, root, 15);
     //object.inOrderTraversal(root);
     //bool result = object.search(root, 12);
